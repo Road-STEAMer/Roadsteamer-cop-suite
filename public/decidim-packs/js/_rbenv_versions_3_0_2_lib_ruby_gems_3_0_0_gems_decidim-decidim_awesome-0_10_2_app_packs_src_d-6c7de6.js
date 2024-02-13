f65eec7bb70119bf13f319b938c4ae8a1c4fb2a4
DanieleNoto <daniele.noto@outlook.com>
2024-02-13 11:35:32 +0100

    look and feel fixed

--- public/decidim-packs/js/_rbenv_versions_3_0_2_lib_ruby_gems_3_0_0_gems_decidim-decidim_awesome-0_10_2_app_packs_src_d-6c7de6.js
+++ public/decidim-packs/js/_rbenv_versions_3_0_2_lib_ruby_gems_3_0_0_gems_decidim-decidim_awesome-0_10_2_app_packs_src_d-6c7de6.js
@@ -1,276 +1,5 @@
-(self["webpackChunkdecidim_app"] = self["webpackChunkdecidim_app"] || []).push([["_rbenv_versions_3_0_2_lib_ruby_gems_3_0_0_gems_decidim-decidim_awesome-0_10_2_app_packs_src_d-6c7de6"],{
-
-/***/ "../.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/gems/decidim-decidim_awesome-0.10.2/app/packs/src/decidim/decidim_awesome/editors/editor.js":
-/*!************************************************************************************************************************************************!*\
-  !*** ../.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/gems/decidim-decidim_awesome-0.10.2/app/packs/src/decidim/decidim_awesome/editors/editor.js ***!
-  \************************************************************************************************************************************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-
 "use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "createMarkdownEditor": function() { return /* binding */ createMarkdownEditor; },
-/* harmony export */   "createQuillEditor": function() { return /* binding */ createQuillEditor; },
-/* harmony export */   "destroyQuillEditor": function() { return /* binding */ destroyQuillEditor; }
-/* harmony export */ });
-/* harmony import */ var src_decidim_editor_linebreak_module__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! src/decidim/editor/linebreak_module */ "./app/packs/src/decidim/editor/linebreak_module.js");
-/* harmony import */ var inscrybmde__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! inscrybmde */ "./node_modules/inscrybmde/src/js/inscrybmde.js");
-/* harmony import */ var inscrybmde__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(inscrybmde__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var europa__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! europa */ "./node_modules/europa/dist/europa.js");
-/* harmony import */ var europa__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(europa__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var inline_attachment_src_inline_attachment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! inline-attachment/src/inline-attachment */ "./node_modules/inline-attachment/src/inline-attachment.js");
-/* harmony import */ var inline_attachment_src_inline_attachment__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(inline_attachment_src_inline_attachment__WEBPACK_IMPORTED_MODULE_3__);
-/* harmony import */ var inline_attachment_src_codemirror_4_inline_attachment__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! inline-attachment/src/codemirror-4.inline-attachment */ "./node_modules/inline-attachment/src/codemirror-4.inline-attachment.js");
-/* harmony import */ var inline_attachment_src_codemirror_4_inline_attachment__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(inline_attachment_src_codemirror_4_inline_attachment__WEBPACK_IMPORTED_MODULE_4__);
-/* harmony import */ var inline_attachment_src_jquery_inline_attachment__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! inline-attachment/src/jquery.inline-attachment */ "./node_modules/inline-attachment/src/jquery.inline-attachment.js");
-/* harmony import */ var inline_attachment_src_jquery_inline_attachment__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(inline_attachment_src_jquery_inline_attachment__WEBPACK_IMPORTED_MODULE_5__);
-/* harmony import */ var highlight_js__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! highlight.js */ "./node_modules/highlight.js/es/index.js");
-/* harmony import */ var highlight_js_styles_github_css__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! highlight.js/styles/github.css */ "./node_modules/highlight.js/styles/github.css");
-/* harmony import */ var src_decidim_editor_clipboard_override__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! src/decidim/editor/clipboard_override */ "./app/packs/src/decidim/editor/clipboard_override.js");
-/* harmony import */ var src_decidim_vendor_image_resize_min__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! src/decidim/vendor/image-resize.min */ "./app/packs/src/decidim/vendor/image-resize.min.js");
-/* harmony import */ var src_decidim_vendor_image_resize_min__WEBPACK_IMPORTED_MODULE_9___default = /*#__PURE__*/__webpack_require__.n(src_decidim_vendor_image_resize_min__WEBPACK_IMPORTED_MODULE_9__);
-/* harmony import */ var src_decidim_vendor_image_upload_min__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! src/decidim/vendor/image-upload.min */ "./app/packs/src/decidim/vendor/image-upload.min.js");
-/* harmony import */ var src_decidim_vendor_image_upload_min__WEBPACK_IMPORTED_MODULE_10___default = /*#__PURE__*/__webpack_require__.n(src_decidim_vendor_image_upload_min__WEBPACK_IMPORTED_MODULE_10__);
-/* harmony import */ var marked__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! marked */ "./node_modules/marked/lib/marked.esm.js");
-function _toConsumableArray(arr) {
-  return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread();
-}
-function _nonIterableSpread() {
-  throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
-}
-function _unsupportedIterableToArray(o, minLen) {
-  if (!o) return;
-  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
-  var n = Object.prototype.toString.call(o).slice(8, -1);
-  if (n === "Object" && o.constructor) n = o.constructor.name;
-  if (n === "Map" || n === "Set") return Array.from(o);
-  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
-}
-function _iterableToArray(iter) {
-  if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter);
-}
-function _arrayWithoutHoles(arr) {
-  if (Array.isArray(arr)) return _arrayLikeToArray(arr);
-}
-function _arrayLikeToArray(arr, len) {
-  if (len == null || len > arr.length) len = arr.length;
-  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
-  return arr2;
-}
-/* eslint-disable require-jsdoc, func-style */
-
-/*
-* Since version 0.25 we follow a different strategy and opt to destroy and override completely the original editor
-* That's because editors are instantiated directly instead of creating a global function to instantiate them
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-var DecidimAwesome = window.DecidimAwesome || {};
-var quillFormats = ["bold", "italic", "link", "underline", "header", "list", "video", "image", "alt", "break", "width", "style", "code", "blockquote", "indent"];
-
-// A tricky way to destroy the quill editor
-function destroyQuillEditor(container) {
-  if (container) {
-    var content = $(container).find(".ql-editor").html();
-    $(container).html(content);
-    $(container).siblings(".ql-toolbar").remove();
-    $(container).find("*[class*='ql-']").removeClass(function (index, className) {
-      return (className.match(/(^|\s)ql-\S+/g) || []).join(" ");
-    });
-    $(container).removeClass(function (index, className) {
-      return (className.match(/(^|\s)ql-\S+/g) || []).join(" ");
-    });
-    if ($(container).next().is("p.help-text")) {
-      $(container).next().remove();
-    }
-  } else {
-    console.error("editor [".concat(container, "] not exists"));
-  }
-}
-function createQuillEditor(container) {
-  var toolbar = $(container).data("toolbar");
-  var disabled = $(container).data("disabled");
-  var allowedEmptyContentSelector = "iframe";
-  var quillToolbar = [["bold", "italic", "underline", "linebreak"], [{
-    list: "ordered"
-  }, {
-    list: "bullet"
-  }], ["link", "clean"], ["code", "blockquote"], [{
-    "indent": "-1"
-  }, {
-    "indent": "+1"
-  }]];
-  var addImage = false;
-  if (toolbar === "full") {
-    quillToolbar = [[{
-      header: [2, 3, 4, 5, 6, false]
-    }]].concat(_toConsumableArray(quillToolbar));
-    if (DecidimAwesome.allow_images_in_full_editor) {
-      quillToolbar.push(["video", "image"]);
-      addImage = true;
-    } else {
-      quillToolbar.push(["video"]);
-    }
-  } else if (toolbar === "basic") {
-    if (DecidimAwesome.allow_images_in_small_editor) {
-      quillToolbar.push(["video", "image"]);
-      addImage = true;
-    } else {
-      quillToolbar.push(["video"]);
-    }
-  } else if (DecidimAwesome.allow_images_in_small_editor) {
-    quillToolbar.push(["image"]);
-    addImage = true;
-  }
-  var modules = {
-    linebreak: {},
-    toolbar: {
-      container: quillToolbar,
-      handlers: {
-        "linebreak": src_decidim_editor_linebreak_module__WEBPACK_IMPORTED_MODULE_0__["default"]
-      }
-    }
-  };
-  var $input = $(container).siblings('input[type="hidden"]');
-  container.innerHTML = $input.val() || "";
-  var token = $('meta[name="csrf-token"]').attr("content");
-  if (addImage) {
-    modules.imageResize = {
-      modules: ["Resize", "DisplaySize"]
-    };
-    modules.imageUpload = {
-      url: DecidimAwesome.editor_uploader_path,
-      method: "POST",
-      name: "image",
-      withCredentials: false,
-      headers: {
-        "X-CSRF-Token": token
-      },
-      callbackOK: function callbackOK(serverResponse, next) {
-        $("div.ql-toolbar").last().removeClass("editor-loading");
-        next(serverResponse.url);
-      },
-      callbackKO: function callbackKO(serverError) {
-        $("div.ql-toolbar").last().removeClass("editor-loading");
-        var msg = serverError && serverError.body;
-        try {
-          msg = JSON.parse(msg).message;
-        } catch (evt) {
-          console.error("Parsing error", evt);
-        }
-        console.error("Image upload error: ".concat(msg));
-        var $p = $("<p class=\"text-alert help-text\">".concat(msg, "</p>"));
-        $(container).after($p);
-        setTimeout(function () {
-          $p.fadeOut(1000, function () {
-            $p.destroy();
-          });
-        }, 3000);
-      },
-      checkBeforeSend: function checkBeforeSend(file, next) {
-        $("div.ql-toolbar").last().addClass("editor-loading");
-        next(file);
-      }
-    };
-  }
-  var quill = new Quill(container, {
-    modules: modules,
-    formats: quillFormats,
-    theme: "snow"
-  });
-  if (disabled) {
-    quill.disable();
-  }
-  quill.on("text-change", function () {
-    var text = quill.getText();
-
-    // Triggers CustomEvent with the cursor position
-    // It is required in input_mentions.js
-    var event = new CustomEvent("quill-position", {
-      detail: quill.getSelection()
-    });
-    container.dispatchEvent(event);
-    if (text === "\n" || text === "\n\n") {
-      $input.val("");
-    } else {
-      $input.val(quill.root.innerHTML);
-    }
-    if ((text === "\n" || text === "\n\n") && quill.root.querySelectorAll(allowedEmptyContentSelector).length === 0) {
-      $input.val("");
-    } else {
-      var emptyParagraph = "<p><br></p>";
-      var cleanHTML = quill.root.innerHTML.replace(new RegExp("^".concat(emptyParagraph, "|").concat(emptyParagraph, "$"), "g"), "");
-      $input.val(cleanHTML);
-    }
-  });
-  // After editor is ready, linebreak_module deletes two extraneous new lines
-  quill.emitter.emit("editor-ready");
-  if (addImage) {
-    var text = $(container).data("dragAndDropHelpText") || DecidimAwesome.texts.drag_and_drop_image;
-    $(container).after("<p class=\"help-text\" style=\"margin-top:-1.5rem;\">".concat(text, "</p>"));
-  }
-
-  // After editor is ready, linebreak_module deletes two extraneous new lines
-  quill.emitter.emit("editor-ready");
-  return quill;
-}
-function createMarkdownEditor(container) {
-  var text = DecidimAwesome.texts.drag_and_drop_image;
-  var token = $('meta[name="csrf-token"]').attr("content");
-  var $input = $(container).siblings('input[type="hidden"]');
-  var $faker = $('<textarea name="faker-inscrybmde"/>');
-  var $form = $(container).closest("form");
-  var europa = new (europa__WEBPACK_IMPORTED_MODULE_2___default())();
-  $faker.val(europa.convert($input.val()));
-  $faker.insertBefore($(container));
-  $(container).hide();
-  var inscrybmde = new (inscrybmde__WEBPACK_IMPORTED_MODULE_1___default())({
-    element: $faker[0],
-    spellChecker: false,
-    renderingConfig: {
-      codeSyntaxHighlighting: true,
-      hljs: highlight_js__WEBPACK_IMPORTED_MODULE_6__["default"]
-    }
-  });
-  $faker[0].InscrybMDE = inscrybmde;
-
-  // Allow image upload
-  if (DecidimAwesome.allow_images_in_markdown_editor) {
-    $(inscrybmde.gui.statusbar).prepend("<span class=\"help-text\" style=\"float:left;margin:0;text-align:left;\">".concat(text, "</span>"));
-    window.inlineAttachment.editors.codemirror4.attach(inscrybmde.codemirror, {
-      uploadUrl: DecidimAwesome.editor_uploader_path,
-      uploadFieldName: "image",
-      jsonFieldName: "url",
-      extraHeaders: {
-        "X-CSRF-Token": token
-      }
-    });
-  }
-
-  // Allow linebreaks
-  marked__WEBPACK_IMPORTED_MODULE_11__.marked.setOptions({
-    breaks: true
-  });
-
-  // convert to html on submit
-  $form.on("submit", function () {
-    // e.preventDefault();
-    $input.val((0,marked__WEBPACK_IMPORTED_MODULE_11__.marked)(inscrybmde.value()));
-  });
-}
-
-/***/ }),
+(self["webpackChunkdecidim_app"] = self["webpackChunkdecidim_app"] || []).push([["_rbenv_versions_3_0_2_lib_ruby_gems_3_0_0_gems_decidim-decidim_awesome-0_10_2_app_packs_src_d-6c7de6"],{
 
 /***/ "../.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/gems/decidim-decidim_awesome-0.10.2/app/packs/src/decidim/decidim_awesome/forms/custom_fields_renderer.js":
 /*!**************************************************************************************************************************************************************!*\
@@ -278,7 +7,6 @@ function createMarkdownEditor(container) {
   \**************************************************************************************************************************************************************/
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": function() { return /* binding */ CustomFieldsRenderer; }
@@ -574,9 +302,8 @@ var CustomFieldsRenderer = /*#__PURE__*/function () {
   \********************************************************************************************************************************************************/
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var src_decidim_decidim_awesome_editors_editor__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! src/decidim/decidim_awesome/editors/editor */ "../.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/gems/decidim-decidim_awesome-0.10.2/app/packs/src/decidim/decidim_awesome/editors/editor.js");
+/* harmony import */ var src_decidim_decidim_awesome_editors_editor__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! src/decidim/decidim_awesome/editors/editor */ "./app/packs/src/decidim/decidim_awesome/editors/editor.js");
 function _typeof(obj) {
   "@babel/helpers - typeof";
 
@@ -883,7 +610,6 @@ window.fbControls.push(function (controlClass, allControlClasses) {
   \*********************************************************************************************************************************************************/
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var src_decidim_decidim_awesome_forms_custom_fields_renderer__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! src/decidim/decidim_awesome/forms/custom_fields_renderer */ "../.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/gems/decidim-decidim_awesome-0.10.2/app/packs/src/decidim/decidim_awesome/forms/custom_fields_renderer.js");
 
@@ -905,16 +631,6 @@ $(function () {
     }
   });
 });
-
-/***/ }),
-
-/***/ "?b2fd":
-/*!********************!*\
-  !*** fs (ignored) ***!
-  \********************/
-/***/ (function() {
-
-/* (ignored) */
 
 /***/ })
 
